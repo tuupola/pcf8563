009d752749b6f048cb8c3f9ea1e70e1a137ade6a
Mika Tuupola <tuupola@appelsiini.net>
2020-04-21 21:48:33 +0700

    Change header block formatting

--- bm8563.h
+++ bm8563.h
@@ -1,7 +1,5 @@
 /*
 
-SPDX-License-Identifier: MIT
-
 MIT License
 
 Copyright (c) 2020 Mika Tuupola
@@ -23,6 +21,13 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+-cut-
+
+This file is part of hardware agnostic I2C driver for BM8563 RTC:
+https://github.com/tuupola/bm8563
+
+SPDX-License-Identifier: MIT
 
 */
 
