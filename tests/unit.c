1d676ce43bcdc433cba95db636f374bd86e2ffc3
Mika Tuupola <tuupola@appelsiini.net>
2025-12-08 06:41:46 +0200

    Add test for PCF8563_ERROR_NOTTY

--- tests/unit.c
+++ tests/unit.c
@@ -302,6 +302,20 @@ should_read_and_write_timer(void)
     PASS();
 }
 
+TEST
+should_return_error_for_invalid_ioctl(void)
+{
+    uint8_t dummy;
+    pcf8563_t bm;
+    bm.read = &mock_i2c_read;
+    bm.write = &mock_i2c_write;
+
+    ASSERT(PCF8563_OK == pcf8563_init(&bm));
+    ASSERT(PCF8563_ERROR_NOTTY == pcf8563_ioctl(&bm, 0x9999, &dummy));
+
+    PASS();
+}
+
 GREATEST_MAIN_DEFS();
 
 int
@@ -322,6 +336,7 @@ main(int argc, char **argv)
     RUN_TEST(should_handle_year_2099);
     RUN_TEST(should_read_and_write_alarm);
     RUN_TEST(should_read_and_write_timer);
+    RUN_TEST(should_return_error_for_invalid_ioctl);
 
     GREATEST_MAIN_END();
 }
